9bff3dab1091d19bba09fedd88b6df6d3d69a4ed
integeek <ambre.giraudlanza@outlook.fr>
2023-07-07 19:43:45 +0200

    ✅ mise en page

--- src/component/page.tsx
+++ src/component/page.tsx
@@ -1,18 +1,18 @@
 function Page() {
   return (
-    <div className="flex items-center justify-center h-screen rounded-[32px] drop-shadow-2xl">
-      <div className="relative max-w-screen-lg bg-gray-200 rounded-lg w-128 h-96">
+
+    <div className="flex items-center justify-center h-screen rounded-[32px] drop-shadow-2xl flex-col md:flex-row">
+      <div className="relative max-w-screen-lg bg-gray-200 rounded-lg w-128 h-96 grid ">
+        <title>Results summary</title>
         <div className=" px-4 float-left w-1/2 h-full rounded-lg bg-gradient-to-b from-light-slate-blue to-light-royal-blue font-hanken">
-          <h1 className="pt-10 text-2xl font-bold text-light-lavender mb-4">Your Results</h1>
-          <div className="flex items-center justify-center w-32 h-32 rounded-full bg-gradient-to-b from-violet-blue to-persian-blue mx-auto mt-auto">
+          <h1 className="pt-10 text-2xl font-bold text-light-lavender mb-4">Your Result</h1>
+          <div className="flex items-center justify-center w-36 h-36 rounded-full bg-gradient-to-b from-violet-blue to-persian-blue mx-auto mt-auto">
             <div className="flex flex-col items-center">
               <p className="text-white text-5xl font-bold">76</p>
               <p className="text-white">of 100</p>
             </div>
           </div>
-
-
-          <p className="font-hanken font-bold mt-6 mb-4 text-2xl text-white">Great</p>
+          <p className="font-hanken font-bold mt-4 mb-2 text-2xl text-white">Great</p>
           <p className="font-hanken text-light-lavender">You scored higher than 65% of the people who have taken these tests.</p>
         </div>
 
@@ -37,7 +37,7 @@ function Page() {
               <p className="m-0 pl-4">Memory</p>
             </div>
             <div className="flex items-center">
-              <div className="text-black">92</div>
+              <div className="text-black ">92</div>
               <div className="text-dark-gray-blue/50">/100</div>
             </div>
           </div>
@@ -59,10 +59,9 @@ function Page() {
               <p className="m-0 pl-4">Visual</p>
             </div>
             <div className="flex items-center">
-              <div className="text-black ">72</div>
+              <div className="text-black">72</div>
               <div className="text-dark-gray-blue/50">/</div>
               <div className="text-dark-gray-blue/50 ">100</div>
-
             </div>
           </div>
 
