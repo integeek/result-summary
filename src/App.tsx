e35b844475070a9a577bb22094d96a3d542538ce
integeek <ambre.giraudlanza@outlook.fr>
2023-07-09 11:55:00 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <div className ="App">
       <BrowserRouter> 
         <Routes>
-          <Route path="/result-summary" element={<Page />} />  
+          <Route path="/result-summary/" element={<Page />} />  
         </Routes>
       </BrowserRouter>
     </div>
